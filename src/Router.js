123ec3208cc4404c0f67a885570a0236fdad97d0
BHwi <bhwi0422@gmail.com>
2022-11-22 19:27:42 +0900

    add : root page

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/Main/Main";
+import Root from "./pages/Root/Root";
 
 export function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Main />} />
+      <Route exact path="/root" element={<Root />} />
+      <Route exact path="*" element={<Main />} />
     </Routes>
   );
 }
